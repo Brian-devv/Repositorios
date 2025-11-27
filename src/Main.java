d2b2c6825515b088296aeb6fbaa2cc4486f10dba
Brian Ramos Veras <brianramosveras@gmail.com>
2025-11-27 12:28:01 -0300

    Primeiro commit com código Java e .gitignore

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("instrucao numero 1");
+        System.out.println("instrucao numero 2");
+        System.out.println("instrucao numero 3");
+        System.out.println("instrucao numero 4");
+    }
+}
